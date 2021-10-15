34a4ae76a8a159a1ace888355efd7ec35e326451
zarshed1997 <92128200+zarshed1997@users.noreply.github.com>
2021-10-15 16:50:10 -0400

    Fixed wishlist enable cssselector

--- src/test/features/WishList.feature
+++ src/test/features/WishList.feature
@@ -1,9 +1,9 @@
 Feature: Wish List
   Scenario: Customer able to use WishList
-    Given Customer is on DemoShop home page book
+    Given Customer is on DemoShop home page wishlist
     When Customer search for a book
     Then Customer is on search page
     When Click on book
-    When Click on add to wishlist
+    When Customer adds item to wishlist
     Then wishlist cart should get updated (0) to (1)
 
--- src/test/java/stepDefinations/WishList.java
+++ src/test/java/stepDefinations/WishList.java
@@ -1,5 +1,6 @@
 package stepDefinations;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.*;
@@ -11,14 +12,19 @@ import static stepDefinations.TestSuiteSetUp.chrome;
 public class WishList {
     @When("Click on book")
     public void click_on_book() throws InterruptedException {
-        chrome.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         chrome.findElement(By.cssSelector(".product-title")).click();
-      
     }
-    @When("Click on add to wishlist")
-    public void click_on_add_to_wishlist() {
+
+    @Given("Customer is on DemoShop home page wishlist")
+    public void customer_is_on_demo_shop_home_page_wishlist() {
+        chrome.get("http://demowebshop.tricentis.com/");
+    }
+
+    @When("Customer adds item to wishlist")
+    public void customer_adds_item_to_wishlist() {
+        chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         chrome.findElement(By.cssSelector("#add-to-wishlist-button-22")).click();
-      
     }
     @Then("wishlist cart should get updated \\({int}) to \\({int})")
     public void wishlist_cart_should_get_updated_to(Integer int1, Integer int2) throws InterruptedException {
